d457819678c61894afaa0064a95180f23131152d
William Crownover <williamcrownover1@gmail.com>
2021-07-22 01:03:01 -0700

    Developed successful post request for adding a new note

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,29 @@
 const notes = require('express').Router();
-const { readFromFile } = require('../utility/fsUtils');
+const uniqid = require('uniqid');
+const { 
+    readFromFile,
+    appendToFile 
+} = require('../utility/fsUtils');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json')
-        .then((data) => res.json(JSON.parse(data)));
+        .then((data) => res.send(JSON.parse(data)));
+});
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    const newNote = {
+        title,
+        text,
+        noteID: uniqid()
+    };
+
+    appendToFile(newNote, './db/db.json');
+
+    res.json('Note added successfully');
 });
 
 module.exports = notes;
--- utility/fsUtils.js
+++ utility/fsUtils.js
@@ -3,4 +3,20 @@ const util = require('util');
 
 const readFromFile = util.promisify(fs.readFile);
 
-module.exports = { readFromFile };
+const writeToFile = (content, filePath) => {
+    fs.writeFile(filePath, content, (err) => {
+        err ? console.log(err) : console.log("Success!")
+    });
+}
+
+const appendToFile = (obj, filePath) => {
+    readFromFile(filePath)
+        .then((data) => {
+            const parsedData = JSON.parse(data);
+            parsedData.push(obj);
+            const dataJSON = JSON.stringify(parsedData, null, 4);
+            writeToFile(dataJSON, filePath);
+        });
+}
+
+module.exports = { readFromFile, appendToFile };
